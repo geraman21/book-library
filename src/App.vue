0c8e0bb073d28604c4994e9dea4dff45dd5234db
German Abdurahmanov <german.abdurahmanov@aldi-sued.com>
2024-08-26 08:37:37 +0200

    Refactor: add book is on the homepage

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <main class="container">
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/front-desk">Front Desk</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </main>
 </template>
